39c62a89f54a58e3aa162d0b44abdf3668afdd76
zukky <34536327+SuzukiDaishi@users.noreply.github.com>
2020-09-02 00:04:07 +0900

    first commit

---
+++ components/organisms/Login.vue
@@ -0,0 +1,32 @@
+<template>
+    <section class="section">
+
+        <form>
+
+            <h1 class="title">ロゴ</h1>
+        
+            <b-field label="Email">
+                <b-input type="email" maxlength="100" />
+            </b-field>
+
+            <b-field label="Password">
+                <b-input type="password" maxlength="100" password-reveal />
+            </b-field>
+
+            <b-button native-type="submit">
+                ログイン
+            </b-button>
+
+        </form>
+
+    </section>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="section">
+    <div class="columns is-centered">
+      <div class="column is-three-quarters">
+        <div class="box">
+          <login />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Login from '@/components/organisms/Login.vue'
+
+export default {
+  components: {
+    Login,
+  }
+}
+</script>
