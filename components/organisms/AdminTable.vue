c6d7241e298446c33d678f091f73256363aff08b
KazuyukiFukuda <kazu.fuku.1003@gmail.com>
2020-09-03 00:44:01 +0900

    add file that are for  admin

---
+++ components/organisms/AdminTable.vue
@@ -0,0 +1,29 @@
+<template>
+    <b-table :data="data" :columns="searchColumns"></b-table>
+</template>
+
+<script>
+export default {
+    props:{
+        data:{
+            type: Array,
+            requiered: true
+        },
+        columns:{
+            type: Array,
+            requiered: true
+        }
+    },
+    mounted() {
+        this.searchColumns = this.columns;
+        for(const num in  this.searchColumns){
+            this.searchColumns[num].searchable = true;
+        }
+    },
+    data(){
+        return {
+            searchColumns: []
+        }
+    }
+}
+</script>
