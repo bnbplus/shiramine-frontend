f34a1341243ed8ebfcbcdab7c348b1c6949ebd25
nunotani123 <nunotani123@gmail.com>
2020-09-03 00:56:13 +0900

    file add

---
+++ components/organisms/Admin_travelers_edit.vue
@@ -0,0 +1,23 @@
+<template>
+    <form>
+        <b-field label="BLEタグ">
+                <b-input v-model="bletag"></b-input>
+        </b-field>
+        <b-field label="名前">
+            <b-input v-model="name"></b-input>
+        </b-field>
+        <b-field label="年齢">
+            <b-input v-model="age"></b-input>
+        </b-field>
+    </form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/user_edit.vue
@@ -0,0 +1,12 @@
+<template>
+</template>
+
+<script>
+export default {
+    layout:'user'
+}
+</script>
+
+<style>
+
+</style>
