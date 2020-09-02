458853f1574f2055ea049601f36fed20f8e24dad
nunotani123 <nunotani123@gmail.com>
2020-09-02 20:08:16 +0900

    shop page

---
+++ pages/shop.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="section">
+    <div class="columns is-centered">
+      <div class="column is-four-fifths">
+        <div class="box">
+          <shop />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Shop from '@/components/organisms/Shop.vue'
+
+export default {
+  components: {
+    Shop,
+  }
+}
+</script>
