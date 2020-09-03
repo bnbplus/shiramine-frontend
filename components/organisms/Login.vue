d88cefc8ad5ba1ad6dee612ec78d872bc8437611
SuzukiDaishi <zukky.rikugame@gmail.com>
2020-09-04 07:01:09 +0900

    login処理の実装

--- components/organisms/Login.vue
+++ components/organisms/Login.vue
@@ -1,16 +1,16 @@
 <template>
     <section class="section">
 
-        <form>
+        <form @submit.prevent="login" action="/">
 
             <h1 class="title">ロゴ</h1>
         
             <b-field label="Email">
-                <b-input type="email" maxlength="100" />
+                <b-input type="email" v-model="email" maxlength="100" />
             </b-field>
 
             <b-field label="Password">
-                <b-input type="password" maxlength="100" password-reveal />
+                <b-input type="password" v-model="password" maxlength="100" password-reveal />
             </b-field>
 
             <b-button native-type="submit">
@@ -24,9 +24,50 @@
 
 <script>
 export default {
+    data() {
+        return {
+            email: '',
+            password: ''
+        }
+    },
+    methods: {
+        /** ログイン処理 */
+        async login() {
+            try {
+                const back = await this.$axios.post('/login', {
+                    email: this.email,
+                    password: this.password 
+                })
+                this.$store.commit('user/setLoginToken', back.data.token)
+                this.$store.commit('user/setUserRole', back.data.role)
+                this.transition()
+            } catch (err) {
+                console.log(err);
+            }
+            this.email = this.password = ''
+        },
+        /** 画面遷移 */
+        transition() {
+            if ( !this.$store.state.user.loginToken || !this.$store.state.user.userRole ) { return }
+            switch ( this.$store.state.user.userRole ) {
+                    case 'admin':
+                        this.$router.push('/admin/travelers')
+                        break
+                    case 'traveller':
+                        this.$router.push('/travelers/view')
+                        break
+                    case 'keyperson':
+                        // FIXME: goto keyperson page
+                        break
+                }
+        },
+        mounted() {
+            // ログイン済みなら遷移
+            this.transition()
+        }
+    }
 }
 </script>
 
 <style>
-
 </style>
