968203d70fab9ebc07c3c6879ab482d5bff881a4
zukky <34536327+SuzukiDaishi@users.noreply.github.com>
2020-09-06 06:48:16 +0900

    API URL 変更

--- pages/admin/travelersedit/_id.vue
+++ pages/admin/travelersedit/_id.vue
@@ -12,7 +12,7 @@ export default {
     async asyncData({store, app, redirect, params}) {
       if ( !store.state.user.loginToken ) { return redirect('/') } // リダイレクト
       try {
-        const res = await app.$axios.$get(`/users/${params.id}`, {
+        const res = await app.$axios.$get(`/user/${params.id}`, {
           headers: {
             'Content-Type': 'application/json;charset=utf-8',
             Authorization: store.state.user.loginToken
